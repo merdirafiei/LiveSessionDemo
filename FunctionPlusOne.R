440ac1c73f1c576b764cfa2790ef6a13c4447271
Merdi <Merdi>
2016-10-24 18:56:20 -0500

    First commit of the main code and function PlusOne

---
+++ FunctionPlusOne.R
@@ -0,0 +1,4 @@
+# This is a demo source code
+PlusOne <- function(t) {
+    return(t + 1)
+}
---
+++ MainCode.R
@@ -0,0 +1,6 @@
+# This is the main code that calls the function
+# Plusone()
+BunchOfNumbers <- c(1, -1, 3, 3.14)
+for (i in 1: length(BunchOfNumbers)) {
+  PlusOne(BunchOfNumbers[i])
+}
