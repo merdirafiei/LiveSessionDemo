9b5bce7a5798ac3581f1e63aeb7348576232bd7c
Merdi Rafiei <merdirafiei@gmail.com>
2016-10-24 18:59:38 -0500

    Added one element to the end of the vector

--- MainCode.R
+++ MainCode.R
@@ -1,6 +1,6 @@
 # This is the main code that calls the function
 # Plusone()
-BunchOfNumbers <- c(1, -1, 3, 3.14)
+BunchOfNumbers <- c(1, -1, 3, 3.14, -5)
 for (i in 1: length(BunchOfNumbers)) {
   PlusOne(BunchOfNumbers[i])
 }
